b69453f72c3fd5e8db88eb65dcd5c9b9686ba813
sjaehnig <sjaehnig@umich.edu>
2020-08-21 17:13:35 -0700

    email_links

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ $(document).ready(function(){
   });    
 
 });
+
+
+function email_hover(name) {
+  document.getElementById("profile_picture_" + name).style.opacity = 0.2;
+  document.getElementById("email_" + name).style.display = "block";
+  // document.getElementById("").style.display = "visible";
+}
+
+function email_off_hover(name) {
+  document.getElementById("email_" + name).style.display = "none";
+  document.getElementById("profile_picture_" + name).style.opacity = 1.0;
+  // document.getElementById("").style.display = "visible";
+}
